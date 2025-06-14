550622e1cac239498641e5c9944ef633a7315975
Hari Shankar <71510856+HariShankar08@users.noreply.github.com>
2025-06-14 10:12:52 +0530

    Update runs

--- src/runs.sh
+++ src/runs.sh
@@ -1,20 +1,324 @@
 #!/bin/bash
 
+# # WMT_FR here, WMT_HI, CNN commented out. (No changes except for DATASET_NAME)
 
-echo "Running Patch Training with K=4, Llama 3.2 1B Instruct on dataset: WMT-FR with batch size 4"
-python main.py --mode patch_peft --model_path meta-llama/Llama-3.2-1B-Instruct --dataset wmt_hi --save_path output/llama32_1b_k4_2p1f --batch_size 8 --num_runs 5 | tee logs/llama32_1b_k4_2p1f_bs4.log
+# Globals
+PATCH_SIZE=8
+BATCH_SIZE=8
+LAMBDA_RATIO=0.8  # May have to change this, based on ablation_runs result. 
+NUM_RUNS=5
+DATASET_NAME="wmt_fr"  
 
-echo "Running Full Training with Llama 3.2 1B Instruct on dataset: WMT-FR with batch size 4"
-python main.py --mode lora_finetune --model_path meta-llama/Llama-3.2-1B-Instruct --dataset wmt_hi --save_path output/llama32_1b_fft --batch_size 8 --num_runs 5 | tee logs/llama32_1b_fft_bs4.log
+MODEL_PATH="meta-llama/Llama-3.2-1B-Instruct"
+MODEL_SHORT='llama32_1b'
 
-echo "Running Patch Training with K=2, Llama 3.2 1B Instruct on dataset: WMT-FR with batch size 4"
-python main.py --mode patch_peft --patch_size 2 --model_path meta-llama/Llama-3.2-1B-Instruct --dataset wmt_hi --save_path output/llama32_1b_k2_2p1f --batch_size 8 --num_runs 5 | tee logs/llama32_1b_k2_2p1f_bs4.log
+# Llama 3.2 1B Instruct
 
-echo "Running Patch Training with K=8, Llama 3.2 1B Instruct on dataset: WMT-FR with batch size 4"
-python main.py --mode patch_peft --patch_size 8 --model_path meta-llama/Llama-3.2-1B-Instruct --dataset wmt_hi --save_path output/llama32_1b_k8_2p1f --batch_size 8 --num_runs 5 | tee logs/llama32_1b_k8_2p1f_bs4.log
+echo "Running LoRA Baseline ($MODEL_PATH) 
+on $DATASET_NAME \
+with Batch Size=$BATCH_SIZE, \ 
+Num Runs=$NUM_RUNS"
 
-echo "Running Patch Training with K=16, Llama 3.2 1B Instruct on dataset: WMT-FR with batch size 4"
-python main.py --mode patch_peft --patch_size 16 --model_path meta-llama/Llama-3.2-1B-Instruct --dataset wmt_hi --save_path output/llama32_1b_k16_2p1f --batch_size 8 --num_runs 5 | tee logs/llama32_1b_k16_2p1f_bs4.log
+python main.py \
+    --mode lora_finetune \
+    --model_path $MODEL_PATH \
+    --dataset $DATASET_NAME \
+    --save_path $MODEL_SHORT'_bs'$BATCH_SIZE \
+    --batch_size $BATCH_SIZE \
+
+echo "Running Patch Training ($MODEL_PATH) on $DATASET_NAME with K=$PATCH_SIZE, \
+Lambda Ratio=$LAMBDA_RATIO, \
+Batch Size=$BATCH_SIZE, \
+Num Runs=$NUM_RUNS"
+python main.py \
+    --mode patch_peft \
+    --model_path $MODEL_PATH \
+    --dataset $DATASET_NAME \
+    --save_path $MODEL_SHORT'_k'$PATCH_SIZE'_l'$LAMBDA_RATIO'_bs'$BATCH_SIZE \
+    --batch_size $BATCH_SIZE \
+    --num_runs $NUM_RUNS \  
+    | tee logs/$MODEL_SHORT'_k'$PATCH_SIZE'_l'$LAMBDA_RATIO'_bs'$BATCH_SIZE.log
+
+# Phi 3.5 Mini Instruct
+
+MODEL_PATH="microsoft/Phi-3.5-mini-instruct"
+MODEL_SHORT='phi35_mini'
+
+
+echo "Running LoRA Baseline ($MODEL_PATH) 
+on $DATASET_NAME \
+with Batch Size=$BATCH_SIZE, \ 
+Num Runs=$NUM_RUNS"
+
+python main.py \
+    --mode lora_finetune \
+    --model_path $MODEL_PATH \
+    --dataset $DATASET_NAME \
+    --save_path $MODEL_SHORT'_bs'$BATCH_SIZE \
+    --batch_size $BATCH_SIZE \
+
+echo "Running Patch Training ($MODEL_PATH) on $DATASET_NAME with K=$PATCH_SIZE, \
+Lambda Ratio=$LAMBDA_RATIO, \
+Batch Size=$BATCH_SIZE, \
+Num Runs=$NUM_RUNS"
+python main.py \
+    --mode patch_peft \
+    --model_path $MODEL_PATH \
+    --dataset $DATASET_NAME \
+    --save_path $MODEL_SHORT'_k'$PATCH_SIZE'_l'$LAMBDA_RATIO'_bs'$BATCH_SIZE \
+    --batch_size $BATCH_SIZE \
+    --num_runs $NUM_RUNS \  
+    | tee logs/$MODEL_SHORT'_k'$PATCH_SIZE'_l'$LAMBDA_RATIO'_bs'$BATCH_SIZE.log
+
+
+# Falcon H1 7B
+
+MODEL_PATH="tiiuae/Falcon-H1-7B-Instruct"
+MODEL_SHORT='falcon7b'
+
+
+echo "Running LoRA Baseline ($MODEL_PATH) 
+on $DATASET_NAME \
+with Batch Size=$BATCH_SIZE, \ 
+Num Runs=$NUM_RUNS"
+
+python main.py \
+    --mode lora_finetune \
+    --model_path $MODEL_PATH \
+    --dataset $DATASET_NAME \
+    --save_path $MODEL_SHORT'_bs'$BATCH_SIZE \
+    --batch_size $BATCH_SIZE \
+
+echo "Running Patch Training ($MODEL_PATH) on $DATASET_NAME with K=$PATCH_SIZE, \
+Lambda Ratio=$LAMBDA_RATIO, \
+Batch Size=$BATCH_SIZE, \
+Num Runs=$NUM_RUNS"
+python main.py \
+    --mode patch_peft \
+    --model_path $MODEL_PATH \
+    --dataset $DATASET_NAME \
+    --save_path $MODEL_SHORT'_k'$PATCH_SIZE'_l'$LAMBDA_RATIO'_bs'$BATCH_SIZE \
+    --batch_size $BATCH_SIZE \
+    --num_runs $NUM_RUNS \  
+    | tee logs/$MODEL_SHORT'_k'$PATCH_SIZE'_l'$LAMBDA_RATIO'_bs'$BATCH_SIZE.log
+
+
+
+# #!/bin/bash
+
+# # Globals
+# PATCH_SIZE=4
+# BATCH_SIZE=8
+# LAMBDA_RATIO=0.6666
+# NUM_RUNS=5
+# DATASET_NAME="wmt_hi"  
+
+# MODEL_PATH="meta-llama/Llama-3.2-1B-Instruct"
+# MODEL_SHORT='llama32_1b'
+
+# # Llama 3.2 1B Instruct
+
+# echo "Running LoRA Baseline ($MODEL_PATH) 
+# on $DATASET_NAME \
+# with Batch Size=$BATCH_SIZE, \ 
+# Num Runs=$NUM_RUNS"
+
+# python main.py \
+#     --mode lora_finetune \
+#     --model_path $MODEL_PATH \
+#     --dataset $DATASET_NAME \
+#     --save_path $MODEL_SHORT'_bs'$BATCH_SIZE \
+#     --batch_size $BATCH_SIZE \
+
+# echo "Running Patch Training ($MODEL_PATH) on $DATASET_NAME with K=$PATCH_SIZE, \
+# Lambda Ratio=$LAMBDA_RATIO, \
+# Batch Size=$BATCH_SIZE, \
+# Num Runs=$NUM_RUNS"
+# python main.py \
+#     --mode patch_peft \
+#     --model_path $MODEL_PATH \
+#     --dataset $DATASET_NAME \
+#     --save_path $MODEL_SHORT'_k'$PATCH_SIZE'_l'$LAMBDA_RATIO'_bs'$BATCH_SIZE \
+#     --batch_size $BATCH_SIZE \
+#     --num_runs $NUM_RUNS \  
+#     | tee logs/$MODEL_SHORT'_k'$PATCH_SIZE'_l'$LAMBDA_RATIO'_bs'$BATCH_SIZE.log
+
+# # Phi 3.5 Mini Instruct
+
+# MODEL_PATH="microsoft/Phi-3.5-mini-instruct"
+# MODEL_SHORT='phi35_mini'
+
+
+# echo "Running LoRA Baseline ($MODEL_PATH) 
+# on $DATASET_NAME \
+# with Batch Size=$BATCH_SIZE, \ 
+# Num Runs=$NUM_RUNS"
+
+# python main.py \
+#     --mode lora_finetune \
+#     --model_path $MODEL_PATH \
+#     --dataset $DATASET_NAME \
+#     --save_path $MODEL_SHORT'_bs'$BATCH_SIZE \
+#     --batch_size $BATCH_SIZE \
+
+# echo "Running Patch Training ($MODEL_PATH) on $DATASET_NAME with K=$PATCH_SIZE, \
+# Lambda Ratio=$LAMBDA_RATIO, \
+# Batch Size=$BATCH_SIZE, \
+# Num Runs=$NUM_RUNS"
+# python main.py \
+#     --mode patch_peft \
+#     --model_path $MODEL_PATH \
+#     --dataset $DATASET_NAME \
+#     --save_path $MODEL_SHORT'_k'$PATCH_SIZE'_l'$LAMBDA_RATIO'_bs'$BATCH_SIZE \
+#     --batch_size $BATCH_SIZE \
+#     --num_runs $NUM_RUNS \  
+#     | tee logs/$MODEL_SHORT'_k'$PATCH_SIZE'_l'$LAMBDA_RATIO'_bs'$BATCH_SIZE.log
+
+
+# # Falcon H1 7B
+
+# MODEL_PATH="tiiuae/Falcon-H1-7B-Instruct"
+# MODEL_SHORT='falcon7b'
+
+
+# echo "Running LoRA Baseline ($MODEL_PATH) 
+# on $DATASET_NAME \
+# with Batch Size=$BATCH_SIZE, \ 
+# Num Runs=$NUM_RUNS"
+
+# python main.py \
+#     --mode lora_finetune \
+#     --model_path $MODEL_PATH \
+#     --dataset $DATASET_NAME \
+#     --save_path $MODEL_SHORT'_bs'$BATCH_SIZE \
+#     --batch_size $BATCH_SIZE \
+
+# echo "Running Patch Training ($MODEL_PATH) on $DATASET_NAME with K=$PATCH_SIZE, \
+# Lambda Ratio=$LAMBDA_RATIO, \
+# Batch Size=$BATCH_SIZE, \
+# Num Runs=$NUM_RUNS"
+# python main.py \
+#     --mode patch_peft \
+#     --model_path $MODEL_PATH \
+#     --dataset $DATASET_NAME \
+#     --save_path $MODEL_SHORT'_k'$PATCH_SIZE'_l'$LAMBDA_RATIO'_bs'$BATCH_SIZE \
+#     --batch_size $BATCH_SIZE \
+#     --num_runs $NUM_RUNS \  
+#     | tee logs/$MODEL_SHORT'_k'$PATCH_SIZE'_l'$LAMBDA_RATIO'_bs'$BATCH_SIZE.log
+
+
+
+
+
+
+# #!/bin/bash
+
+# # Globals
+# PATCH_SIZE=4
+# BATCH_SIZE=8
+# LAMBDA_RATIO=8
+# NUM_RUNS=5
+# DATASET_NAME="wmt_cnn"  
+
+# MODEL_PATH="meta-llama/Llama-3.2-1B-Instruct"
+# MODEL_SHORT='llama32_1b'
+
+# # Llama 3.2 1B Instruct
+
+# echo "Running LoRA Baseline ($MODEL_PATH) 
+# on $DATASET_NAME \
+# with Batch Size=$BATCH_SIZE, \ 
+# Num Runs=$NUM_RUNS"
+
+# python main.py \
+#     --mode lora_finetune \
+#     --model_path $MODEL_PATH \
+#     --dataset $DATASET_NAME \
+#     --save_path $MODEL_SHORT'_bs'$BATCH_SIZE \
+#     --batch_size $BATCH_SIZE \
+
+# echo "Running Patch Training ($MODEL_PATH) on $DATASET_NAME with K=$PATCH_SIZE, \
+# Lambda Ratio=$LAMBDA_RATIO, \
+# Batch Size=$BATCH_SIZE, \
+# Num Runs=$NUM_RUNS"
+# python main.py \
+#     --mode patch_peft \
+#     --model_path $MODEL_PATH \
+#     --dataset $DATASET_NAME \
+#     --save_path $MODEL_SHORT'_k'$PATCH_SIZE'_l'$LAMBDA_RATIO'_bs'$BATCH_SIZE \
+#     --batch_size $BATCH_SIZE \
+#     --num_runs $NUM_RUNS \  
+#     | tee logs/$MODEL_SHORT'_k'$PATCH_SIZE'_l'$LAMBDA_RATIO'_bs'$BATCH_SIZE.log
+
+# # Phi 3.5 Mini Instruct
+
+# MODEL_PATH="microsoft/Phi-3.5-mini-instruct"
+# MODEL_SHORT='phi35_mini'
+
+
+# echo "Running LoRA Baseline ($MODEL_PATH) 
+# on $DATASET_NAME \
+# with Batch Size=$BATCH_SIZE, \ 
+# Num Runs=$NUM_RUNS"
+
+# python main.py \
+#     --mode lora_finetune \
+#     --model_path $MODEL_PATH \
+#     --dataset $DATASET_NAME \
+#     --save_path $MODEL_SHORT'_bs'$BATCH_SIZE \
+#     --batch_size $BATCH_SIZE \
+
+# echo "Running Patch Training ($MODEL_PATH) on $DATASET_NAME with K=$PATCH_SIZE, \
+# Lambda Ratio=$LAMBDA_RATIO, \
+# Batch Size=$BATCH_SIZE, \
+# Num Runs=$NUM_RUNS"
+# python main.py \
+#     --mode patch_peft \
+#     --model_path $MODEL_PATH \
+#     --dataset $DATASET_NAME \
+#     --save_path $MODEL_SHORT'_k'$PATCH_SIZE'_l'$LAMBDA_RATIO'_bs'$BATCH_SIZE \
+#     --batch_size $BATCH_SIZE \
+#     --num_runs $NUM_RUNS \  
+#     | tee logs/$MODEL_SHORT'_k'$PATCH_SIZE'_l'$LAMBDA_RATIO'_bs'$BATCH_SIZE.log
+
+
+# # Falcon H1 7B
+
+# MODEL_PATH="tiiuae/Falcon-H1-7B-Instruct"
+# MODEL_SHORT='falcon7b'
+
+
+# echo "Running LoRA Baseline ($MODEL_PATH) 
+# on $DATASET_NAME \
+# with Batch Size=$BATCH_SIZE, \ 
+# Num Runs=$NUM_RUNS"
+
+# python main.py \
+#     --mode lora_finetune \
+#     --model_path $MODEL_PATH \
+#     --dataset $DATASET_NAME \
+#     --save_path $MODEL_SHORT'_bs'$BATCH_SIZE \
+#     --batch_size $BATCH_SIZE \
+
+# echo "Running Patch Training ($MODEL_PATH) on $DATASET_NAME with K=$PATCH_SIZE, \
+# Lambda Ratio=$LAMBDA_RATIO, \
+# Batch Size=$BATCH_SIZE, \
+# Num Runs=$NUM_RUNS"
+# python main.py \
+#     --mode patch_peft \
+#     --model_path $MODEL_PATH \
+#     --dataset $DATASET_NAME \
+#     --save_path $MODEL_SHORT'_k'$PATCH_SIZE'_l'$LAMBDA_RATIO'_bs'$BATCH_SIZE \
+#     --batch_size $BATCH_SIZE \
+#     --num_runs $NUM_RUNS \  
+#     | tee logs/$MODEL_SHORT'_k'$PATCH_SIZE'_l'$LAMBDA_RATIO'_bs'$BATCH_SIZE.log
+
+
+
+
+
+
+
 
 
 
